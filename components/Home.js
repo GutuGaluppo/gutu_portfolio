c87443f40de605f0ce64fe0b91a1d32db981d111
Gutu Galuppo <galuppodev@gmail.com>
2020-11-26 09:52:09 +0100

    cleaned home

--- components/Home.js
+++ components/Home.js
@@ -1,57 +1,12 @@
 import Layout from '../components/layout'
-import styles from '../styles/Home.module.css'
-
 import Navbar from '../components/Navbar'
-
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons'
+import JumpingLetters from '../components/JumpingLetters'
 
 export default function Home() {
 	return (
 		<Layout>
-			<div className={styles.main}>
-				<section className={styles.home}>
-				<Navbar/>
-					<aside className={styles.aside__r}>
-						<p className={styles.instagram}>Instagram</p>
-						<div className={styles.goldSphere}></div>
-						<p className={styles.twitter}>Twitter</p>
-					</aside>
-
-					<div className={styles.centerDiv}>
-
-						<div className={styles.info}>
-							<p>Gutu galuppo</p>
-							<h5>
-								Frontend web developer
-							<span></span>
-							</h5>
-							<h1>Design is thinking made visual.</h1>
-							<div className={styles.view__profile}>
-								<h6>View Profile</h6>
-								<button>
-									<FontAwesomeIcon icon={faChevronRight} />
-								</button>
-							</div>
-						</div>
-
-						<div className={styles.pic}>
-							<img src="/images/gutu_portrait.png" alt="Gutu profile picture" className={styles.portrait} />
-						</div>
-					</div>
-
-					<aside className={styles.asside__l}>
-						<FontAwesomeIcon icon={faChevronLeft} />
-						<p>01</p>
-						<hr />
-						<p>50</p>
-						<FontAwesomeIcon icon={faChevronRight} />
-					</aside>
-					<div className={styles.footer}>
-						<h1>Software Developer</h1>
-					</div>
-				</section>
-			</div>
+			<Navbar />
+			<JumpingLetters />
 		</Layout>
 	)
 }
