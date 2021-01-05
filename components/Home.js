ac455da2911c2111520dfd656c6055d47a418467
Gutu Galuppo <galuppodev@gmail.com>
2021-01-05 17:02:46 +0100

    removed Navbar from Home component

--- components/Home.js
+++ components/Home.js
@@ -1,11 +1,9 @@
 import Layout from '../components/layout'
-import Navbar from '../components/Navbar'
 import JumpingLetters from '../components/JumpingLetters'
 
 export default function Home() {
 	return (
 		<Layout>
-			<Navbar />
 			<JumpingLetters />
 		</Layout>
 	)
